f70eec9045083ef88a66c3f9043d7b852e4340a0
gabatk <gabriela.trybuszek@gmail.com>
2023-03-12 21:40:54 +0100

    GoalItem prepared.

--- src/components/GoalItem.js
+++ src/components/GoalItem.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import './GoalItem.css';
 
-const GoalItem = () => {};
+const GoalItem = () => {
+	return (
+		<div>
+			<li className='goal-item'>Feed the dog</li>
+			<li className='goal-item'>Pay bills</li>
+			<li className='goal-item'>Send the package</li>
+		</div>
+	);
+};
 
 export default GoalItem;
--- src/components/GoalsList.js
+++ src/components/GoalsList.js
@@ -1,11 +1,14 @@
 import React from "react";
-
+import GoalItem from "./GoalItem";
 const GoalsList = () => {
+
+    return (
 <div>
     <ul>
         <GoalItem />
     </ul>
 </div>
+)
 }
 
 export default GoalsList;
